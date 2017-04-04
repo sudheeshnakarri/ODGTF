f414a46e3732419a897b7a5ada6dd56c366f6eef
sudheeshnakarri <sudheeshna.karri@osterhoutgroup.com>
2017-04-04 00:52:06 -0700

    Update xmlparsor_v2.py

--- xmlparsor_v2.py
+++ xmlparsor_v2.py
@@ -1,3 +1,15 @@
+# xmlparsor.py
+#
+#
+# Created by Sudheeshna Karri on 4/1/17.
+#
+#TODO:
+#
+#More effitient parsor
+#Send the test details ,status requests ,result requests etc in command structure form
+#
+
+
 from xml.dom import minidom
 import socket
 import sys
@@ -5,8 +17,9 @@ import cPickle
 import time
 from collections import namedtuple
 
-# read the xml file
-xmldoc = minidom.parse('simpletest_v2.xml')
+# Parse the xml file
+
+xmldoc = minidom.parse('simpletest_P.xml')
 modules = xmldoc.getElementsByTagName('module')
 
 for s in modules:
@@ -27,31 +40,29 @@ for s in modules:
           HandleData[0].firstChild.data,
           IsData[0].firstChild.data, DataPath[0].firstChild.data, Log[0].firstChild.data, Filename[0].firstChild.data,
           TimeOut[0].firstChild.data, LoopCount[0].firstChild.data]
+          
     size=sys.getsizeof( Execution)
 #    print(size)
 #    print(Execution) # printing unicode string
 
-   # TCP Connection
-
-    TCP_IP = '192.168.1.110'
-    TCP_PORT = 5007
+   # TCP Commnication
+    TCP_IP = '192.168.160.148'
+    TCP_PORT = 54673
     BUFFER_SIZE = 4096
     count=0
     
 #Test details sent via tcp connection
-
-#cpickle works but have to deserialize .Check it out
+#cpickle works but have to deserializeat recieving end.Check it out ???
 
     MESSAGE =HandleData[0].firstChild.data
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
     s.sendall(MESSAGE)
-    #s.sendall(MESSAGE)
     data = s.recv(BUFFER_SIZE)
     s.close()
     print "Daemon:", data
     status =1
-#has to be recieved by an other thread
+    # request for test execution status. IF status is DONE EXECUTION, request for result and read value
     while status == 1 :
         STATUS ="Status"
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,7 +74,7 @@ for s in modules:
         time.sleep(1)
         if data == "DONE EXECUT" :
            status =0
-
+#Request for result
     STATUS ="Result"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
